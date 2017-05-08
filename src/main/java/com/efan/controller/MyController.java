5e427a13fb70f31d75a6166cdfda3832e8f2204a
454258314 <454258314@qq.com>
2017-05-08 13:36:26 +0800

    测试接口

--- src/main/java/com/efan/controller/MyController.java
+++ src/main/java/com/efan/controller/MyController.java
@@ -1,9 +1,13 @@
 package com.efan.controller;
 
+import com.efan.appservice.iservice.IMyTapeService;
+import com.efan.controller.dtos.MyTapeDto;
+import com.efan.core.entity.MyTape;
 import com.efan.core.page.ActionResult;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,8 +19,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/my")
 public class MyController {
+    private IMyTapeService _mytapeService;
     @Autowired
-    public MyController(){
+    public MyController(IMyTapeService mytapeService){
+        this._mytapeService=mytapeService;
 
     }
     /*获取我的歌单列表*/
@@ -25,5 +31,21 @@ public class MyController {
     @RequestMapping(value  ="/songs" ,method = RequestMethod.POST)
     public ActionResult MySongs(String openId){
         return  new ActionResult();
+    }
+
+
+    /*创建我的原唱歌曲*/
+    @ApiOperation(value="创建我的原唱歌曲", notes="我的模块接口")
+    @RequestMapping(value  ="/modifytape" ,method = RequestMethod.POST)
+    public ActionResult ModifyTape(@RequestBody MyTapeDto input){
+         MyTape result=_mytapeService.ModifyMyTape(input);
+         return  new ActionResult(result);
+    }
+    /*通知设备上传文件并获取到唯一id*/
+    @ApiOperation(value="通知设备上传文件并获取到唯一id", notes="我的模块接口")
+    @RequestMapping(value  ="/notifyupload" ,method = RequestMethod.POST)
+    public ActionResult NotifyUploadSongs(@RequestBody MyTapeDto input){
+        MyTape result=_mytapeService.ModifyMyTape(input);
+        return  new ActionResult(result);
     }
 }
---
+++ src/main/java/com/efan/controller/dtos/MyTapeDto.java
@@ -0,0 +1,22 @@
+package com.efan.controller.dtos;
+
+
+
+/**
+ * Created by 45425 on 2017/5/8.
+ */
+public class MyTapeDto {
+
+    public Long id;
+    //唯一标识
+    public String songKey;
+    //歌曲名称
+    public  String  songName;
+    //演唱者
+    public  String singer;
+    //原唱
+    public  String originalSinger;
+    //时长
+    public  String songtime;
+
+}
