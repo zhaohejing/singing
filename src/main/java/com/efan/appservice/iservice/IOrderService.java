308a0030e9544155a9fc657547965fc16b3b369a
454258314 <454258314@qq.com>
2017-05-08 18:09:51 +0800

    接口完善

--- src/main/java/com/efan/appservice/iservice/IOrderService.java
+++ src/main/java/com/efan/appservice/iservice/IOrderService.java
@@ -4,9 +4,16 @@ import com.efan.controller.dtos.RemoteDto;
 import com.efan.core.page.PageModel;
 import com.efan.core.page.ResultModel;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by 45425 on 2017/5/8.
  */
 public interface IOrderService {
-    ResultModel<RemoteDto> GetRemoteList(PageModel input);
+     ResultModel<RemoteDto> GetRemoteList(String pointName, PageModel input) ;
+    /**
+     * 获取包厢列表
+     * */
+     ResultModel<RemoteDto> GetCoupeList(int remoteId, PageModel input) ;
 }
--- src/main/java/com/efan/appservice/service/OrderService.java
+++ src/main/java/com/efan/appservice/service/OrderService.java
@@ -21,16 +21,34 @@ public class OrderService implements IOrderService {
      public OrderService(IOrderRepository orderRepository){
          this._orderRepository=orderRepository;
      }
-    public ResultModel<RemoteDto> GetRemoteList(PageModel input) {
+     /**
+      * 获取门店列表
+     * */
+    public ResultModel<RemoteDto> GetRemoteList(String pointName, PageModel input) {
          List<RemoteDto> result = new ArrayList<RemoteDto>();
-        result.add(new RemoteDto(1,"大望路1","大望路36号1",11));
-        result.add(new RemoteDto(2,"大望路2","大望路36号2",22));
-        result.add(new RemoteDto(3,"大望路3","大望路36号3",33));
-        result.add(new RemoteDto(4,"大望路4","大望路36号4",44));
-        result.add(new RemoteDto(5,"大望路5","大望路36号5",55));
-        result.add(new RemoteDto(6,"大望路6","大望路36号6",66));
-        result.add(new RemoteDto(7,"大望路7","大望路36号7",77));
-        result.add(new RemoteDto(8,"大望路8","大望路36号8",88));
+        result.add(new RemoteDto(1,"大望路1","大望路36号1",11,0));
+        result.add(new RemoteDto(2,"大望路2","大望路36号2",22,0));
+        result.add(new RemoteDto(3,"大望路3","大望路36号3",33,0));
+        result.add(new RemoteDto(4,"大望路4","大望路36号4",44,0));
+        result.add(new RemoteDto(5,"大望路5","大望路36号5",55,0));
+        result.add(new RemoteDto(6,"大望路6","大望路36号6",66,0));
+        result.add(new RemoteDto(7,"大望路7","大望路36号7",77,0));
+        result.add(new RemoteDto(8,"大望路8","大望路36号8",88,0));
+        return  new ResultModel<RemoteDto>(result,(long)result.size());
+    }
+    /**
+     * 获取包厢列表
+     * */
+    public ResultModel<RemoteDto> GetCoupeList(int remoteId, PageModel input) {
+        List<RemoteDto> result = new ArrayList<RemoteDto>();
+        result.add(new RemoteDto(1,"box1","大望路36号1",11,1));
+        result.add(new RemoteDto(2,"box2","大望路36号2",22,1));
+        result.add(new RemoteDto(3,"box3","大望路36号3",33,1));
+        result.add(new RemoteDto(4,"box4","大望路36号4",44,1));
+        result.add(new RemoteDto(5,"box5","大望路36号5",55,1));
+        result.add(new RemoteDto(6,"box6","大望路36号6",66,1));
+        result.add(new RemoteDto(7,"box7","大望路36号7",77,1));
+        result.add(new RemoteDto(8,"box8","大望路36号8",88,1));
         return  new ResultModel<RemoteDto>(result,(long)result.size());
     }
 }
